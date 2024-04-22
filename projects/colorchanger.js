cb7502602e41d6dd2f7a9ff7ae204efd5f6aa311
Souradeep Maitra <114827308+codersouro28@users.noreply.github.com>
2024-04-22 16:21:34 +0000

    project 2 done

---
+++ projects/colorchanger.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.button');
+// console.log(buttons);
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  // console.log(button);
+  button.addEventListener('click', (e) => {
+    // console.log(e)
+    // console.log(e.target)
+    // if (e.target.id === "grey")
+    body.style.backgroundColor = e.target.id;
+  });
+});
